a1c7b593b5863c3e09b6a1b3241486fb2c972797
junyong231 <bagjunyong14@gmail.com>
2024-10-28 00:40:50 +0900

    main js 수정

--- webapp/main/oho_main.js
+++ webapp/main/oho_main.js
@@ -1,4 +1,4 @@
-
+//색깔 고르기
 $('.find_color_list span').on("click", function() {
     // 클릭한 요소에 'on' 클래스 토글
     $(this).toggleClass('on');
@@ -9,6 +9,8 @@ $('.find_color_list div.find_color_btn').on("click", function() {
 });
 
 
+
+//이미지 호버
 $(".items-swiper-wrapper li").on({
     mouseenter: function() {
         //alert("asd")
@@ -18,4 +20,13 @@ $(".items-swiper-wrapper li").on({
         $(this).find(".hover_img").css("opacity" ,"0")
         $(this).find(".thumb_img").css("opacity" ,"1")
     }
+});
+
+// 주간 베스트 카테고리 선택
+
+$(".cate_tab span").on("click" , function() {
+    //alert("as")
+    $(this).siblings().removeClass("on")
+    $(this).addClass("on")
+
 });
